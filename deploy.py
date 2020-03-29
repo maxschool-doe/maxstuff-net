8326d3d0ea03bb6a4be298d8299f11864e531ea0
maxrichardburgess <maxrichardburgess@users.noreply.github.com>
2020-03-29 16:06:02 +1100

    Deployment to web now a script

--- deploy.py
+++ deploy.py
@@ -2,8 +2,9 @@ server = 'files.000webhost.com'
 username = 'MaxStuff'
 password = '6@vLK!Sn99GZwvn'
 myPath = 'maxstuff.net'
-import os.path, os
+import os.path, os.path
 from ftplib import FTP, error_perm
+
 
 host = 'localhost'
 port = 21
@@ -12,13 +13,20 @@ ftp = FTP()
 ftp.connect(server,21)
 ftp.login(username,password)
 filenameCV = myPath
-
+def valid_file(name):
+    x = True
+    if name.startswith("SyncToy") and name.endswith(".dat"):
+        x = False
+    return x
 def placeFiles(ftp, path):
     for name in os.listdir(path):
         localpath = os.path.join(path, name)
         if os.path.isfile(localpath):
-            print("STOR", name, localpath)
-            ftp.storbinary('STOR ' + name, open(localpath,'rb'))
+            if valid_file(name):
+                print("STOR", name, localpath)
+                ftp.storbinary('STOR ' + name, open(localpath,'rb'))
+            else:
+                print("SKIP", name, localpath)
         elif os.path.isdir(localpath):
             print("MKD", name)
 
@@ -38,4 +46,4 @@ def placeFiles(ftp, path):
 
 placeFiles(ftp, filenameCV)
 
-ftp.quit()
+##ftp.quit()
