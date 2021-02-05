ca12f98eb6706328f61be4eb45c5337a57c2cf8c
maxrichardburgess <maxrichardburgess@users.noreply.github.com>
2021-02-05 16:42:18 +1100

    h

---
+++ maxstuff.net/public_html/sufferfesttofit/script.js
@@ -0,0 +1,42 @@
+var testData = `Warm Up
+
+
+10 minutes @ RPE 1-2 -- 145-120% of RTP -- HR <70-87%
+
+
+Activation 1
+Number of Sets
+3
+Rest Between Sets
+30s
+Interval Distance Per Set:
+75m sprint
+Interval Effort:
+RPE 10 --<85% of RTP --HR NA
+Recovery:
+RPE <2 -->145% of RTP --HR <70%
+
+
+Main Effort
+Number of Sets
+4
+Rest Between Sets
+3 minutes
+Interval Distance Per set
+1K
+Interval Effort:
+RPE 5-6 --120-110% of RTP --HR 87-95%
+Final 100m @ RPE 10 --<85% of RTP --HR 105%
+Recovery:
+RPE 1-2 --<145% of RTP --HR <70%
+
+
+Note: Pace it such that you are increasing your speed for the last 100m and not slowing at the end.
+
+
+Extra Volume
+NA
+
+
+Cool Down
+5-10 minutes @ RPE 2 -- 145% of FTP -- HR <70%`;
